a1825d4f19a019bd6fcb7af5cece3771836d45d9
Marthplays <ahnynib.mr@gmail.com>
2024-10-22 21:18:22 -0300

    Exercicios Java

--- ExerciciosJS/js/app.js
+++ ExerciciosJS/js/app.js
@@ -40,7 +40,7 @@ for (let i = 0; i < 10; i++) {
     }
 }
 */
-
+/*ex3
 let numero
 
 numero = Math.floor(Math.random() * 501)
@@ -49,8 +49,38 @@ console.log(numero)
 
 if (numero >= 100 && numero <= 300) {
     console.log("O número está no intervalo!")
+}*/
+/*
+ex4
+let pesoTerra = prompt("Digite o peso que deseja converter:")
+let planeta = prompt("Digite para qual planeta que deseja converter:")
+let pesoConvertido
+
+if (planeta.toLowerCase() == "mercúrio") {
+    pesoConvertido = (pesoTerra/100)*0.37
+} 
+
+if (planeta.toLowerCase() == "vênus") {
+    pesoConvertido = (pesoTerra/100)*0.88
 }
 
+if (planeta.toLowerCase() == "marte") {
+    pesoConvertido = (pesoTerra/100)*0.38
+}
 
+if (planeta.toLowerCase() == "júpiter") {
+    pesoConvertido = (pesoTerra/100)*2.64
+}
+
+if (planeta.toLowerCase() == "saturno") {
+    pesoConvertido = (pesoTerra/100)*1.15
+}
+
+if (planeta.toLowerCase() == "urano") {
+    pesoConvertido = (pesoTerra/100)*1.17
+}
+
+alert(`A conversão é: ${pesoConvertido}`)
+*/
 
 
